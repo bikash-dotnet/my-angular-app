2e5369905a0722a83e1b500d8cbda2d845762ec0
bikash-dotnet <ranjanbikash1990@gmail.com>
2026-02-15 19:15:00 +0530

    Update Route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,7 @@ import { Listing } from './listing/listing';
 import { FormTabs } from './form-tabs/form-tabs';
 
 export const routes: Routes = [
+	{ path: '', redirectTo: 'listing', pathMatch: 'full' },
 	{ path: 'listing', component: Listing },
 	{ path: 'formtab', component: FormTabs },	
 ];
